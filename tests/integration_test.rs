2cf65d88a7de8ec7cb2b0066aa41e0cfd443b599
Magnus Bergmark <magnus.bergmark@gmail.com>
2016-09-19 20:44:37 +0200

    Add integration test for thread usage
    I'd like to make this step easier.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -9,3 +9,23 @@ fn it_works() {
     assert_eq!(pixel.0, 550.0);
     assert_eq!(pixel.1, 319.0);
 }
+
+#[test]
+fn it_can_be_shared_over_threads() {
+    use std::thread;
+    use std::sync::Arc;
+
+    let projection = Arc::new(GoogleProjection::new());
+    let mut threads = vec![];
+
+    for _ in 0..3 {
+        let reference = projection.clone();
+        threads.push(thread::spawn(move || {
+            reference.from_ll_to_pixel(&(13.2, 55.9), 2).unwrap();
+        }));
+    }
+
+    for thread in threads {
+        let _ = thread.join();
+    }
+}
