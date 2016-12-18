6badee93634c93ebb1d63940061cec691edda89c
Magnus Bergmark <magnus.bergmark@gmail.com>
2016-12-18 18:29:16 +0100

    Run rustfmt on project

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ pub fn from_ll_to_pixel<T: Coord>(ll: &T, zoom: usize) -> Option<T> {
         let d = c / 2.0;
         let e = ((d + ll.x() * bc) + 0.5).floor();
         let f = ll.y().to_radians().sin().max(-0.9999).min(0.9999);
-        let g = ((d + 0.5 * ((1.0+f)/(1.0-f)).ln() * -cc) + 0.5).floor();
+        let g = ((d + 0.5 * ((1.0 + f) / (1.0 - f)).ln() * -cc) + 0.5).floor();
 
         Some(T::with_xy(e, g))
     } else {
@@ -75,8 +75,12 @@ pub trait Coord {
 }
 
 impl Coord for (f64, f64) {
-    fn x(&self) -> f64 { self.0 }
-    fn y(&self) -> f64 { self.1 }
+    fn x(&self) -> f64 {
+        self.0
+    }
+    fn y(&self) -> f64 {
+        self.1
+    }
 
     fn with_xy(x: f64, y: f64) -> (f64, f64) {
         (x, y)
@@ -90,8 +94,7 @@ mod test {
     const EPSILON: f64 = 1e-10;
 
     fn float_pair_close(pair: &(f64, f64), expected: &(f64, f64)) -> bool {
-        ((pair.0 - expected.0).abs() < EPSILON) &&
-            ((pair.1 - expected.1).abs() < EPSILON)
+        ((pair.0 - expected.0).abs() < EPSILON) && ((pair.1 - expected.1).abs() < EPSILON)
     }
 
     #[test]
@@ -103,21 +106,19 @@ mod test {
 
     #[test]
     fn it_projects_to_pixels() {
-        let answers = vec![
-            ((0.0, 0.0), 0, (128.0, 128.0)),
-            ((0.0, 0.0), 1, (256.0, 256.0)),
-            ((0.0, 0.0), 29, (6.8719476736e10, 6.8719476736e10)),
+        let answers = vec![((0.0, 0.0), 0, (128.0, 128.0)),
+                           ((0.0, 0.0), 1, (256.0, 256.0)),
+                           ((0.0, 0.0), 29, (6.8719476736e10, 6.8719476736e10)),
 
-            ((0.0, 1.0), 0, (128.0, 127.0)),
-            ((1.0, 0.0), 0, (129.0, 128.0)),
-            ((1.0, 1.0), 0, (129.0, 127.0)),
+                           ((0.0, 1.0), 0, (128.0, 127.0)),
+                           ((1.0, 0.0), 0, (129.0, 128.0)),
+                           ((1.0, 1.0), 0, (129.0, 127.0)),
 
-            ((5.5, 5.5), 5, (4221.0, 3971.0)),
+                           ((5.5, 5.5), 5, (4221.0, 3971.0)),
 
-            ((100.0, 54.0), 12, (815559.0, 336679.0)),
+                           ((100.0, 54.0), 12, (815559.0, 336679.0)),
 
-            ((-45.0, 12.0), 6, (6144.0, 7642.0)),
-        ];
+                           ((-45.0, 12.0), 6, (6144.0, 7642.0))];
 
         for answer in answers {
             let ll = answer.0;
@@ -126,33 +127,30 @@ mod test {
 
             let actual = super::from_ll_to_pixel(&ll, zoom).unwrap();
 
-            assert!(
-                float_pair_close(&actual, &expected),
-                format!(
-                    "Expected {:?} at zoom {} to be {:?} but was {:?}",
-                    &ll, zoom, &expected, &actual
-                )
-            );
+            assert!(float_pair_close(&actual, &expected),
+                    format!("Expected {:?} at zoom {} to be {:?} but was {:?}",
+                            &ll,
+                            zoom,
+                            &expected,
+                            &actual));
         }
     }
 
     #[test]
     fn it_projects_to_longlat() {
-        let answers = vec![
-            ((128.0, 128.0), 0, (0.0, 0.0)),
-            ((256.0, 256.0), 1, (0.0, 0.0)),
-            ((6.8719476736e10, 6.8719476736e10), 29, (0.0, 0.0)),
+        let answers = vec![((128.0, 128.0), 0, (0.0, 0.0)),
+                           ((256.0, 256.0), 1, (0.0, 0.0)),
+                           ((6.8719476736e10, 6.8719476736e10), 29, (0.0, 0.0)),
 
-            ((128.0, 127.0), 0, (0.0, 1.4061088354351594)),
-            ((129.0, 128.0), 0, (1.40625, 0.0)),
-            ((129.0, 127.0), 0, (1.40625, 1.4061088354351594)),
+                           ((128.0, 127.0), 0, (0.0, 1.4061088354351594)),
+                           ((129.0, 128.0), 0, (1.40625, 0.0)),
+                           ((129.0, 127.0), 0, (1.40625, 1.4061088354351594)),
 
-            ((20.0, 19.0), 0, (-151.875, 82.11838360691269)),
+                           ((20.0, 19.0), 0, (-151.875, 82.11838360691269)),
 
-            ((78.0, 78.0), 12, (-179.9732208251953, 85.04881808980566)),
+                           ((78.0, 78.0), 12, (-179.9732208251953, 85.04881808980566)),
 
-            ((-67.0, -100.0), 6, (-181.47216796875, 85.2371040233303)),
-        ];
+                           ((-67.0, -100.0), 6, (-181.47216796875, 85.2371040233303))];
 
         for answer in answers {
             let pixel = answer.0;
@@ -161,26 +159,19 @@ mod test {
 
             let actual = super::from_pixel_to_ll(&pixel, zoom).unwrap();
 
-            assert!(
-                float_pair_close(&actual, &expected),
-                format!(
-                    "Expected {:?} at zoom {} to be {:?} but was {:?}",
-                    &pixel, zoom, &expected, &actual
-                )
-            );
+            assert!(float_pair_close(&actual, &expected),
+                    format!("Expected {:?} at zoom {} to be {:?} but was {:?}",
+                            &pixel,
+                            zoom,
+                            &expected,
+                            &actual));
         }
     }
 
     #[test]
     fn it_returns_none_when_zooming_too_far() {
-        assert_eq!(
-            super::from_ll_to_pixel(&(0.0, 0.0), 30),
-            None
-        );
+        assert_eq!(super::from_ll_to_pixel(&(0.0, 0.0), 30), None);
 
-        assert_eq!(
-            super::from_pixel_to_ll(&(0.0, 0.0), 30),
-            None
-        );
+        assert_eq!(super::from_pixel_to_ll(&(0.0, 0.0), 30), None);
     }
 }
